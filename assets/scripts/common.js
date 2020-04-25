4cd9dc34bcd6db8ce3e427cd986da161be48a1bc
Core Dumper <voidnull@github.com>
2020-04-24 20:13:15 -0700

    set styles for timer

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -57,7 +57,7 @@ function toMMSS(ms) {
     var sec = '' + Math.floor(ms % 60);
     var min = '' + Math.floor(ms / 60)
     
-    return min.padStart(2,'0') + ':' + sec.padStart(2,'0')   
+    return min.padStart(2,'0') + ' : ' + sec.padStart(2,'0')
 }
 
 /**********************
